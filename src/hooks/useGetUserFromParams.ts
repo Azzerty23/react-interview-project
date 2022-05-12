f998421d0e2f6833477de229129def4c521c63a6
augustin <azzerty23@hotmail.fr>
2022-05-12 19:44:01 +0200

    fix(useGetUserFromParams): add plain users object in depedencies array

--- src/hooks/useGetUserFromParams.ts
+++ src/hooks/useGetUserFromParams.ts
@@ -1,21 +1,19 @@
-import { useAppDispatch } from '@app/hooks';
 import { FetchStatus } from '@data/enum';
-import { fetchUsers } from '@slices/usersSlice';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useFetchUsers from './useFetchUsers';
 
 const useGetUserFromParams = () => {
+  const [currentUser, setCurrentUser] = useState<User>();
   const { userId } = useParams();
   const users = useFetchUsers();
-  const dispatch = useAppDispatch();
-  const [currentUser, setCurrentUser] = useState<User>();
 
   useEffect(() => {
-    if (users.status === FetchStatus.idle) dispatch(fetchUsers());
-    const user = users.value.find(({ id }) => id.toString() === userId);
-    setCurrentUser(user);
-  }, [dispatch, users.status, userId]);
+    if (users.status === FetchStatus.succeed) {
+      const user = users.value.find(({ id }) => id.toString() === userId);
+      setCurrentUser(user);
+    }
+  }, [users, userId]);
 
   return currentUser;
 };
